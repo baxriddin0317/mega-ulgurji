bb58b6d2cef36838d7d162e0748fbe820fa8076f
baxriddin0317 <baxriddinxojaaxmadaliyev@gmail.com>
2025-07-10 20:48:45 +0500

    add icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,16 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "MegaHome ulgurji",
   description: "Naqd bo'lsa ulgurji bo'laqolsin!",
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+  },
 };
 
 export default function RootLayout({

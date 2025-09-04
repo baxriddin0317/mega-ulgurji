ff2dbd1ce0f56498b8da66f3616f6e876df2c6ef
baxriddin0317 <baxriddinxojaaxmadaliyev@gmail.com>
2025-09-04 21:03:30 +0500

    fix img errors

--- components/client/ProductCard.tsx
+++ components/client/ProductCard.tsx
@@ -11,11 +11,11 @@ interface ProductProps {
 
 const ProductCard = ({product}: ProductProps) => {
   const { isAuthenticated } = useAuthStore();
-
+  
   return (
     <div className='cursor-pointer group'>
       <div className='relative w-full h-64 overflow-hidden rounded-xl mb-3'>
-        <Image className='absolute object-cover group-hover:scale-105 transition-all duration-500' src={product.productImageUrl[0].url} fill alt='' />
+        {product.productImageUrl && product.productImageUrl.length > 0 ? <Image className='absolute object-cover group-hover:scale-105 transition-all duration-500' src={product.productImageUrl[0].url} fill alt='' /> : <div className='absolute size-full bg-gray-200 flex items-center justify-center text-gray-500 text-xs'>Rasm yo&apos;q</div>}
       </div>
       <Link href={`/product/${product.id}`}>
         <h3 className='font-bold text-lg md:text-xl line-clamp-1'>{product.title}</h3>

d3f7c6c85ecab585c7702b53e4051a0b16c47e64
baxriddin0317 <baxriddinxojaaxmadaliyev@gmail.com>
2026-01-24 14:24:59 +0500

    fix logo

--- components/admin/MobileMenu.tsx
+++ components/admin/MobileMenu.tsx
@@ -4,13 +4,16 @@ import {
 } from "@/components/ui/sheet"
 import Menu from "./Menu";
 import Image from "next/image";
+import Link from "next/link";
 
 const MobileMenu = () => {
   
   return (
     <SheetContent side='left' className="w-64 p-4">
-      <SheetTitle className="text-xl font-bold mb-6">
-        <Image className="absolute size-full object-cover" src="/images/BRAND-LOGO.png" fill alt="site logo" />
+      <SheetTitle className="text-xl font-bold">
+        <Link href={'/'} className="relative flex w-56 h-20">
+          <Image className="absolute object-cover" src="/images/BRAND-LOGO.png" fill alt="site logo" />
+        </Link>
       </SheetTitle>
       <Menu />
     </SheetContent>

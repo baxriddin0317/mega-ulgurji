074066fad78539443c69042ae9bf9507f66e2f07
baxriddin0317 <baxriddinxojaaxmadaliyev@gmail.com>
2026-01-24 14:11:52 +0500

    add logo

--- components/admin/MobileMenu.tsx
+++ components/admin/MobileMenu.tsx
@@ -3,12 +3,15 @@ import {
   SheetTitle,
 } from "@/components/ui/sheet"
 import Menu from "./Menu";
+import Image from "next/image";
 
 const MobileMenu = () => {
   
   return (
     <SheetContent side='left' className="w-64 p-4">
-      <SheetTitle className="text-xl font-bold mb-6">Logo</SheetTitle>
+      <SheetTitle className="text-xl font-bold mb-6">
+        <Image className="absolute size-full object-cover" src="/images/BRAND-LOGO.png" fill alt="site logo" />
+      </SheetTitle>
       <Menu />
     </SheetContent>
   )
